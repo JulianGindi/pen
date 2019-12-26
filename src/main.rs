f885d81be0aed1045d2ef0ea71ee33a7e13a8d90
Julian Gindi <julian@gindi.io>
2019-12-26 06:55:07 -0800

    clearning screen on start

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
-    println!("Here is your pen. Begin inputting text...");
     // We will start by storing all inputted text to a single large String type
     let mut text = String::new();
+
+    // Clear terminal screen
+    print!("{}[2J", 27 as char);
+    io::stdout().flush().unwrap();
 
     io::stdin()
         .read_line(&mut text)
